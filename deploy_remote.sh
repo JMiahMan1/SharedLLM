43305fd638261a3e2d26f02ca8983abf5bd1e4fa
jmiahman1 <jmiahman@gmail.com>
2025-12-06 11:02:22 -0700

    chore: Add deployment script

---
+++ deploy_remote.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# deploy_remote.sh
+# Usage: ./deploy_remote.sh [user@machine_ip] [path_to_app]
+
+HOST="${1:-jeremiah@192.168.2.211}"
+DIR="${2:-/home/jeremiah/Summers Drive/Code/SharedLLM}"
+
+echo "Deploying to $HOST:$DIR..."
+
+ssh "$HOST" "cd \"$DIR\" && git fetch origin && git checkout timer && git pull origin timer && docker compose restart"
+
+echo "Deployment complete."
