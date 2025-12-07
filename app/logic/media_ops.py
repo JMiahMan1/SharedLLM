4e085ce6d8e2c5538d30d1993df79aef75912cee
jmiahman1 <jmiahman@gmail.com>
2025-12-06 17:57:10 -0700

    fix: Add 'turn X to [color]' pattern to color command regex
    User says 'turn lamp to blue' not 'set lamp blue'
    Added second regex pattern to catch this common phrasing

--- app/logic/media_ops.py
+++ app/logic/media_ops.py
@@ -46,7 +46,9 @@ REGEX_INTENT_MAP = {
     r"\bgo back\b|\bback\b": "nav_back",
     r"\bgo home\b|\bhome\b": "nav_home",
     r"\bselect\b|\benter\b|\bok\b": "nav_enter",
+    # Color control: matches "set/change/make X color" OR "turn X to color"
     r"\b(set|change|make).+(color|colour|red|blue|green|purple|orange|yellow|pink|white|warm|cool)": "set_color",
+    r"\bturn\s+.+\s+to\s+(red|blue|green|purple|orange|yellow|pink|white|warm|cool)": "set_color",
     r"\b(dim|darken|lower)\b": "dim",
     r"\b(brighten|brighter|increase)\b": "brighten",
     r"\b(brightness|bright|set.+\d+%)": "set_brightness",
