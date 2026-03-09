380c801158a316bf8f6f0a859477b9ae49198b52
Jeremiah <JMiahman@Gmail.com>
2026-03-09 13:40:32 -0700

    config: update announcement blacklist - remove Office TV, add LG WebOS TV

--- app/settings.py
+++ app/settings.py
@@ -101,7 +101,7 @@ INFORMATIONAL_INTENTS = [
 ]
 
 # --- Announcements ---
-ANNOUNCEMENT_BLACKLIST = os.getenv("ANNOUNCEMENT_BLACKLIST", "media_player.office_tv,media_player.media_player_office_tv").split(",")
+ANNOUNCEMENT_BLACKLIST = os.getenv("ANNOUNCEMENT_BLACKLIST", "media_player.lg_webos_tv_un6955zuf_airplay").split(",")
 # Clean up whitespace
 ANNOUNCEMENT_BLACKLIST = [x.strip() for x in ANNOUNCEMENT_BLACKLIST if x.strip()]
 
