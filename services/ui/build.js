93efae4206da417b6d56b1f6968408408267b510
Jeremiah <JMiahman@Gmail.com>
2026-05-04 22:35:13 -0700

    debug: explicitly log internal bundler errors

--- services/ui/build.js
+++ services/ui/build.js
@@ -6,6 +6,9 @@ async function runBuild() {
     console.log('Build successful!');
   } catch (err) {
     console.error('--- BUILD FAILED ---');
+    if (err.errors) {
+      console.log('INTERNAL ERRORS:', JSON.stringify(err.errors, null, 2));
+    }
     console.dir(err, { depth: null });
     process.exit(1);
   }
