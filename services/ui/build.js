6e1028a765310a5ca94fb80def4801452da89103
Jeremiah <JMiahman@Gmail.com>
2026-05-04 22:35:53 -0700

    debug: expose full error object properties

--- services/ui/build.js
+++ services/ui/build.js
@@ -6,10 +6,15 @@ async function runBuild() {
     console.log('Build successful!');
   } catch (err) {
     console.error('--- BUILD FAILED ---');
+    console.log('Error Name:', err.name);
+    console.log('Error Message:', err.message);
     if (err.errors) {
-      console.log('INTERNAL ERRORS:', JSON.stringify(err.errors, null, 2));
+      console.log('Internal Errors:', JSON.stringify(err.errors, null, 2));
     }
-    console.dir(err, { depth: null });
+    // Log all properties including non-enumerable ones
+    for (const key of Object.getOwnPropertyNames(err)) {
+      console.log(`Property [${key}]:`, err[key]);
+    }
     process.exit(1);
   }
 }
